575729c4fdfb7e57394527b2a31bfd2d802cebb5
Daniel Villalba <dvillalba@tiempodevelopment.com>
2016-12-11 23:02:57 -0600

    -Adding Xam.Plugin.Media to control the takepicture and the take video feature on xamarin forms. -Adding the Octane.Xam.Videoplayer that allows to reproduce the video just taken -Formating the main page as TabbedPage

--- MediaPluginSample/MediaPluginSample/MainPage.xaml.cs
+++ MediaPluginSample/MediaPluginSample/MainPage.xaml.cs
@@ -8,40 +8,13 @@ using Xamarin.Forms;
 
 namespace MediaPluginSample
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
 
-            takePhoto.Clicked += async (sender, args) =>
-            {
-                await CrossMedia.Current.Initialize();
 
-                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-                {
-                    await DisplayAlert("No Camera", ":( No camera available.", "OK");
-                    return;
-                }
-
-                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
-                {
-                    Directory = "Sample",
-                    Name = "test.jpg"
-                });
-
-                if (file == null)
-                    return;
-
-                await DisplayAlert("File Location", file.Path, "OK");
-
-                image.Source = ImageSource.FromStream(() =>
-                {
-                    var stream = file.GetStream();
-                    file.Dispose();
-                    return stream;
-                });
-            };
         }
 
     }
---
+++ MediaPluginSample/MediaPluginSample/TakeVideoPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MediaPluginSample
+{
+    public partial class TakeVideoPage : ContentPage
+    {
+        public TakeVideoPage()
+        {
+            InitializeComponent();
+            takeVideo.Clicked += async (sender, args) =>
+            {
+                await CrossMedia.Current.Initialize();
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakeVideoSupported)
+                {
+                    await DisplayAlert("No Video support", "Cant capture video", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakeVideoAsync(new Plugin.Media.Abstractions.StoreVideoOptions
+                {
+                    Directory = "Sample"
+                });
+
+                if (file == null) return;
+
+                await DisplayAlert("File Location", file.Path, "OK");
+
+                video.Source = file.Path;
+            };
+        }
+    }
+}
