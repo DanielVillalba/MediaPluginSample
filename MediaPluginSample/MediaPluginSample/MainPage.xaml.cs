5169ceea4f12876e322a86776808ce3bfe291522
Daniel Villalba <dvillalba@tiempodevelopment.com>
2016-12-11 21:35:49 -0600

    Adding a button to start the camera and and image to display the captured image

--- MediaPluginSample/MediaPluginSample/MainPage.xaml.cs
+++ MediaPluginSample/MediaPluginSample/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Media;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,36 @@ namespace MediaPluginSample
         public MainPage()
         {
             InitializeComponent();
+
+            takePhoto.Clicked += async (sender, args) =>
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "test.jpg"
+                });
+
+                if (file == null)
+                    return;
+
+                await DisplayAlert("File Location", file.Path, "OK");
+
+                image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+            };
         }
+
     }
 }
